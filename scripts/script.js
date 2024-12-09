64d6fd6985b942bf541ae42b6a416e5fdb412fc6
jlcambraia <joaoluiz.cambraia@gmail.com>
2024-12-09 16:11:25 -0300

    feat: adiciona funcionalidade de validação dos formulários e resetar dados de validação quando popup for aberto

--- scripts/script.js
+++ scripts/script.js
@@ -204,3 +204,13 @@ popups.forEach((popup) => {
     }
   });
 });
+
+// Validação dos formulários
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__save-button",
+  inactiveButtonClass: "popup__save-button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_hidden",
+});
