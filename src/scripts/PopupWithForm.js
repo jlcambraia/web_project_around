23c4e331d6f59ca78cfd8d9bd4fcbc1b32027214
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-02-10 12:43:23 -0300

    refactor: melhora classe PopupWithForm

--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -1,37 +1,52 @@
 import Popup from "./Popup.js";
-import { resetPopup } from "./utils.js";
 
 export default class PopupWithForm extends Popup {
   constructor(popupSelector, popupSubmit) {
     super(popupSelector);
     this._popupSubmit = popupSubmit;
+    this._form = this._popup.querySelector("form");
+    this._inputs = Array.from(this._form.querySelectorAll("input"));
+    this._button = this._form.querySelector("button");
+    this._setEventListeners();
   }
 
   _getInputValues() {
-    const popupInputs = this._popup.querySelectorAll(".popup__input");
     const popupValues = {};
 
-    for (let input of popupInputs) {
+    this._inputs.forEach((input) => {
       popupValues[input.name] = input.value;
-    }
+    });
+
     return popupValues;
   }
 
-  setEventListeners() {
-    super.setEventListeners();
+  _setEventListeners() {
+    super._setEventListeners();
 
-    const form = this._popup.querySelector(".popup__form");
-    const saveButton = form.querySelector(".popup__save-button");
-
-    form.addEventListener("submit", (evt) => {
+    this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
       const popupValues = this._getInputValues();
-      this._popupSubmit(popupValues, saveButton);
+      this._popupSubmit(popupValues);
+    });
+  }
+
+  _resetForms() {
+    this._form.reset();
+    this._inputs.forEach((input) => {
+      input.classList.remove("popup__input_type_error");
+      input.nextElementSibling.classList.add("popup__input-error_hidden");
     });
   }
 
   close() {
     super.close();
-    resetPopup();
+    this._resetForms();
+  }
+
+  open() {
+    super.open();
+
+    this._button.setAttribute("disabled", true);
+    this._button.classList.add("popup__save-button_disabled");
   }
 }
